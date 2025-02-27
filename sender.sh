3c31943f8ef5df5fbc7b23624c2ec7c0337a4a90
Mihai <mihaifcp44279@gmail.com>
2025-02-27 20:46:07 +0000

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 port=$1
-path=/home/$(whoami)/sage/sage_rl/rl_module
 id=$2
 finish_time=$3
 scheme="pure"
 max_it=0
-
+path=$4
+path="$path/sage_rl/rl_module"
 $path/sage-server-mahimahi $port $path $id $finish_time $scheme
 
 echo "Finished."
