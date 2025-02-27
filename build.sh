fe776bd1bf2b0b4308124df5532f6eae7ea6ceff
Mihai <mihaifcp44279@gmail.com>
2025-02-27 20:46:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $CURRENT_DIR
 virtualenv --python=python3.6 ~/venvpy36
 source ~/venvpy36/bin/activate
 
-cd ~/sage
+cd $CURRENT_DIR
 pip install --no-deps -r requirements.txt
 pip install -r requirements.txt
 pip install -e .
 deactivate
 
-cd ~/sage/sage_rl
+cd sage_rl
 bash build.sh
 bash set_sysctl.sh
 
-cd ~/sage/sage_rl
+cd "$CURRENT_DIR/sage_rl"
 bash cp_models.sh
